b5ec6afd001c5dd60761b7574d2e9c0a9b2ce031
Calvin Tirrell <calvintirrell@gmail.com>
2019-01-14 19:52:30 -0800

    Added relevant files to the project repository
    This is the initial commit for the project. Only a basic interface and look were done so far.

---
+++ Love Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Love Calculator
+//
+//  Created by Calvin Tirrell on 1/14/19.
+//  Copyright © 2019 CT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
